7a44fd9f1fbf64a42fd0c179c1a5cced73a918b9
Mark Allen Ramirez <markallenramirez@gmail.com>
2025-10-28 16:48:45 +0400

    Update tests

--- Angular/src/app/app.component.spec.ts
+++ Angular/src/app/app.component.spec.ts
@@ -1,35 +1,41 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { DxDataGridModule } from 'devextreme-angular/ui/data-grid';
+import { DxCardViewModule } from 'devextreme-angular';
 import { AppComponent } from './app.component';
+import { ScreenService } from './screen.service';
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
+  let mockBreakpointObserver: jasmine.SpyObj<BreakpointObserver>;
+
   beforeEach(async () => {
+    const breakpointObserverSpy = jasmine.createSpyObj('BreakpointObserver', ['observe', 'isMatched']);
+    breakpointObserverSpy.observe.and.returnValue(of({ matches: false, breakpoints: {} }));
+    breakpointObserverSpy.isMatched.and.returnValue(false);
+
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
+        DxDataGridModule,
+        DxCardViewModule,
       ],
       declarations: [
         AppComponent,
       ],
+      providers: [
+        ScreenService,
+        { provide: BreakpointObserver, useValue: breakpointObserverSpy }
+      ],
     }).compileComponents();
+
+    mockBreakpointObserver = TestBed.inject(BreakpointObserver) as jasmine.SpyObj<BreakpointObserver>;
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it('should have as title \'angular-test\'', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-test');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-test app is running!');
   });
 });
